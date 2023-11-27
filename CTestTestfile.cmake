3f8b475458ce01f20f5acd502efd06a57dc04577
Daerean Legrand <frankrogerstg@gmail.com>
2023-11-27 02:08:48 -0500

    Initial commit

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/daerean/Codes/Universidad/octavo/paralela/implementation/examples/06_splitK_gemm
+# Build directory: /home/daerean/Codes/Universidad/octavo/paralela/implementation/build/examples/06_splitK_gemm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[ctest_examples_06_splitk_gemm]=] "/home/daerean/Codes/Universidad/octavo/paralela/implementation/build/examples/06_splitK_gemm/06_splitK_gemm")
+set_tests_properties([=[ctest_examples_06_splitk_gemm]=] PROPERTIES  DISABLED "OFF" _BACKTRACE_TRIPLES "/home/daerean/Codes/Universidad/octavo/paralela/implementation/CMakeLists.txt;759;add_test;/home/daerean/Codes/Universidad/octavo/paralela/implementation/examples/CMakeLists.txt;72;cutlass_add_executable_tests;/home/daerean/Codes/Universidad/octavo/paralela/implementation/examples/06_splitK_gemm/CMakeLists.txt;31;cutlass_example_add_executable;/home/daerean/Codes/Universidad/octavo/paralela/implementation/examples/06_splitK_gemm/CMakeLists.txt;0;")
